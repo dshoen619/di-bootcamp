b44857dc1b7682e2f3802b4fb6df250e393c5f51
David Shoen <dbshoen@gmail.com>
2022-07-03 17:46:07 +0300

    w3d1

---
+++ Week3/Day1/Dom_exercise.js
@@ -0,0 +1,19 @@
+// let first_try=document.getElementById("page");
+// let text = first_try.firstElementChild;
+// console.log(text);
+
+// let text_2=first_try.firstChild;
+// console.log(text_2);
+
+// let No_2= first_try.lastElementChild;
+// console.log(No_2);
+
+// let No_2_2= first_try.lastChild;
+// console.log(No_2_2);
+
+// let No_3 = first_try.nextElementSibling;
+// console.log(No_3);
+
+let exercise_1=document.getElementsByTagName("div");
+let text=exercise_1.firstElementChild;
+console.log(exercise_1);
---
+++ Week3/Day1/exercise_2.js
@@ -0,0 +1,4 @@
+
+let exercise_2=document.getElementsByTagName("div");
+let John=exercise_2[0].nextElementSibling.children[0];
+let remove_john=John.remove();
