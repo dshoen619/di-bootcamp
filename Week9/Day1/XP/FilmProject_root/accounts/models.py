a5db73ef3cbce5385e357048bd6895cc1c7bd60b
David Shoen <dbshoen@gmail.com>
2022-08-17 17:27:47 +0300

    week9

--- Week9/Day1/XP/FilmProject_root/accounts/models.py
+++ Week9/Day1/XP/FilmProject_root/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserProfile(models.Model):
+    hobbies = models.CharField(max_length=200, null=True)
+    image = models.URLField(null = True)
+    address = models.CharField(max_length=100, null = True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.user.username
--- Week9/Day1/XP/FilmProject_root/accounts/views.py
+++ Week9/Day1/XP/FilmProject_root/accounts/views.py
@@ -1,3 +1,86 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User, Group
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from .forms import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
+
+def signup(request):
+    context = {'form': UserCreationForm}
+    if request.method == 'POST':
+        print("POST")
+        form_filled = UserCreationForm(request.POST)
+        if form_filled.is_valid():
+            
+            form_filled.save(commit = False)
+
+            username = form_filled.cleaned_data.get('username')
+            password = form_filled.cleaned_data.get('password1')
+            # Authentiacate
+            user = authenticate(username=username, password=password)
+            
+            user = form_filled.save()
+
+            UserProfile.objects.create(user_id = user.id)
+
+            regulars = Group.objects.get(name='Regulars')
+            regulars.user_set.add(user)
+
+            login(request, user)
+            return redirect('update_profile')
+
+        else:
+            return render(request, 'signup.html', {'form': form_filled})
+
+    return render(request, 'signup.html', context)
+
+def signin(request):
+    if request.user.is_authenticated:
+        return redirect('homepage')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # Authernticate
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            # login takes request and the user associated
+            login(request, user)
+            return redirect('homepage')
+        else:
+            return render(request, 'signin.html', {'form': AuthenticationForm(request.POST)})
+    
+    else:
+        return render(request, 'signin.html', {'form': AuthenticationForm})
+
+
+def signout(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('signin')
+
+
+def update_profile(request):
+    
+    profile = request.user.userprofile
+    form = ProfileForm(request.POST or None, instance=profile)
+    context = {'form': form} 
+
+    if form.is_valid():
+        form.save()
+        return redirect('profile')
+
+    return render(request, 'update_profile.html', context)
+
+
+def profile(request):
+
+    user = request.user
+    profile = user.userprofile     
+    context = {'profile': profile}
+
+    return render(request, 'profile.html', context)
+
+
